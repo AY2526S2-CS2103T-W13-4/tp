2ea37570a41c76c1f93b4c2d7793a11c4f2da158
tehmiw <tehmingwei1@gmail.com>
2026-04-03 04:01:27 +0800

    Fix how student duplicates are checked

--- src/test/java/seedu/coursepilot/model/CoursePilotTest.java
+++ src/test/java/seedu/coursepilot/model/CoursePilotTest.java
@@ -52,7 +52,7 @@ public class CoursePilotTest {
         List<Student> newStudents = Arrays.asList(ALICE, editedAlice);
         CoursePilotStub newData = new CoursePilotStub(newStudents);
 
-        assertThrows(DuplicateStudentException.class, () -> coursePilot.resetData(newData));
+        coursePilot.resetData(newData);
     }
 
     @Test
@@ -76,7 +76,7 @@ public class CoursePilotTest {
         coursePilot.addStudent(ALICE);
         Student editedAlice = new StudentBuilder(ALICE).withMatriculationNumber(VALID_MATRIC_BOB)
                 .withTags(VALID_TAG_HUSBAND).build();
-        assertTrue(coursePilot.hasStudent(editedAlice));
+        assertFalse(coursePilot.hasStudent(editedAlice));
     }
 
     @Test
